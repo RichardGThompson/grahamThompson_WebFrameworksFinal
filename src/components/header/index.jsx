5dcc093876643ee9e27533fba79b5592d7205c96
Richard Thompson <graham.thompson.613@gmail.com>
2021-12-13 15:11:42 -0500

    Added logout function to the header.

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,27 @@
 import './styles.css';
+import{ Link } from 'react-router-dom';
 
 export const Header = (props) => {
+    
     return(
         <div className="header-wrapper">
-            <div className="profile-picture-container">
-                {/* TODO Replace this image with the real profile picture */}
-                <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" alt="" />
+            <div className="left-wrapper">
+                <div className="profile-picture-container">
+                    {/* TODO Replace this image with the real profile picture */}
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" alt="" />
+                </div>
+                <div className="page-name">
+                    <h1>Browse</h1>
+                </div>
             </div>
-            <div className="page-name">
-                <h1>Browse</h1>
+
+            <div className="right-wrapper">
+                <ul>
+                    {/* <li className="header-link">My Account</li> */}
+                    <Link to="/my-account"></Link>
+                    <Link className="header-link" to="/login" onClick={() => props.logoutFunction()}>Logout</Link>
+                    {/* <li >Logout</li> */}
+                </ul>
             </div>
         </div>
     );
